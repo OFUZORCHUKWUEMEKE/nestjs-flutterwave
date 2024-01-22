ef45fb04061b17609b93b11225901e8dc384ec41
Ofuzor chukwuemeke <uniben2018@gmail.com>
2024-01-22 14:41:45 +0100

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CustomersModule } from './customers/customers.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { WalletModule } from './wallet/wallet.module';
 
 @Module({
-  imports: [],
+  imports: [CustomersModule, TransactionsModule, WalletModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/customers/schema/customer.schema.ts
@@ -0,0 +1,6 @@
+
+
+// @Schema()
+// export class Customer{
+
+// }
