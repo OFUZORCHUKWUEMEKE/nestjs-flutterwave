20ef573abe13663199afab8c6a58b718d941f67a
Ofuzor chukwuemeke <uniben2018@gmail.com>
2024-01-23 13:34:44 +0100

    schema update

--- src/customers/schema/customer.schema.ts
+++ src/customers/schema/customer.schema.ts
@@ -1,7 +1,21 @@
-import { Schema } from "@nestjs/mongoose";
-
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import {Document} from "mongoose"
 
 @Schema({ timestamps: true })
-export class Customer {
-   
+export class Customer extends Document{
+    _id:any;
+
+    @Prop({})
+    first_name:string
+
+    @Prop({})
+    last_name:string
+
+    @Prop({})
+    address:string
+
+    @Prop({})
+    balance:string
 }
+
+export const CustomerSchema = SchemaFactory.createForClass(Customer)
